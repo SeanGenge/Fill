ef2a50e6437e4ad34895607a65f30d3be79106ad
Sean Genge <SeanGenge@users.noreply.github.com>
2018-09-06 14:35:37 +1000

    Added instructions

--- scripts/Container.jsx
+++ scripts/Container.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import '../css/style.css';
 import { Board } from './Board.jsx';
 import { Control } from './Control.jsx';
+import { Instructions } from './Instructions.jsx';
 
 export let DIFF = {
 	easy: 2,
@@ -69,6 +70,7 @@ export class Container extends React.Component {
 		return (
 			<div style={{overflow: "auto", display: "inline-block", background: "Lavender", padding: "10px", border: "2px solid purple"}}>
 				<h1 className="title">Fill</h1>
+				<Instructions />
 				<Board difficulty={this.state.difficulty} fillType={this.state.fillType} numShapes={numShapes} updateBoard={this.state.updateBoard} toggleUpdateBoard={this.toggleUpdateBoard} scramble={this.state.scramble} handleScramble={this.scramble} />
 				<Control difficulty={this.state.difficulty} fillType={this.state.fillType} width={this.state.width} height={this.state.height} onHandleChangeInt={this.onHandleChangeInt} toggleUpdateBoard={this.toggleUpdateBoard} handleScramble={this.scramble} onHandleChange={this.onHandleChange} />
 			</div>
